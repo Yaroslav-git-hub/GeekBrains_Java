7c4955fc505c5eedde53711db920b984906049a6
Ярополк <115994717+Yaroslav-git-hub@users.noreply.github.com>
2023-05-13 11:20:39 +0300

    start

---
+++ Lesson2/HomeWork2.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ Lesson2/Lecture2.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class Lecture2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
